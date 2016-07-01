d8ac683ac6f4e03cf7b5e05831c805f9dba78eb2
Rahul Gupta <neor007idev@gmail.com>
2016-07-01 15:31:10 +0530

    project update 2

--- view/js/slide.js
+++ view/js/slide.js
@@ -2,15 +2,15 @@ var slideIndex1 = 0;
 carousel1();
 
 function carousel1() {
-	var i;
-	var x = document.getElementsByClassName("mySlides1");
-	for (i = 0; i < x.length; i++) {
-		x[i].style.display = "none";
-	}
-	slideIndex1++;
-	if (slideIndex1 > x.length) {slideIndex1 = 1}
-	x[slideIndex1-1].style.display = "block";
-	setTimeout(carousel1, 2000); // Change image every 2 seconds
+    var i;
+    var x = document.getElementsByClassName("mySlides1");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
+    }
+    slideIndex1++;
+    if (slideIndex1 > x.length) {slideIndex1 = 1}
+    x[slideIndex1-1].style.display = "block";
+    setTimeout(carousel1, 2000); // Change image every 2 seconds
 }
 
 var slideIndex2 = 0;
@@ -27,3 +27,4 @@ function carousel2() {
 	x[slideIndex2-1].style.display = "block";
 	setTimeout(carousel2, 2000); // Change image every 2 seconds
 }
+
